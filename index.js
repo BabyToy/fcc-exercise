871f405859e8ddcf74782de748ab4dd7eba36775
Ian Bautista <victor.jackal@gmail.com>
2023-10-20 15:36:03 +0800

    update GET /api/users/:_id/logs to date string

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.get("/api/users/:_id/logs", async (req, res) => {
     return {
       description: log.description,
       duration: log.duration,
-      date: log.date.toString(),
+      date: log.date.toDateString(),
       username: log.user.username,
     };
   });
